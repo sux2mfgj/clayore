dc67620f71f266879b6c8c7484569e0e690f7fa3
sux2mfgj <sux2mfgj@gmail.com>
2020-08-22 14:38:59 +0900

    Initial commit

---
+++ src/main/java/dev/sux2mfgj/clayore/ClayOre.java
@@ -0,0 +1,63 @@
+package com.sux2mfgj.clayore;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod("clayore")
+public class ClayOre
+{
+    public static final String modid = "clayore";
+
+    // Directly reference a log4j logger.
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static ClayOreBlock clayOreBlock;
+    public static Item clayOreBlockItem;
+
+    public ClayOre()
+    {
+        clayOreBlock = new ClayOreBlock();
+        clayOreBlockItem = new BlockItem(clayOreBlock,
+                new Item.Properties().group(ItemGroup.BUILDING_BLOCKS))
+                .setRegistryName(clayOreBlock.getRegistryName());
+
+
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents
+    {
+        @SubscribeEvent
+        public static void registerBlocks(RegistryEvent.Register<Block> event)
+        {
+            event.getRegistry().register(clayOreBlock);
+        }
+
+        @SubscribeEvent
+        public static void registerItems(RegistryEvent.Register<Item> event)
+        {
+            event.getRegistry().register(clayOreBlockItem);
+        }
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        OreGeneration.setupBiomeFeature();
+    }
+}
---
+++ src/main/java/dev/sux2mfgj/clayore/ClayOreBlock.java
@@ -0,0 +1,42 @@
+package com.sux2mfgj.clayore;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.ToolType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.storage.loot.LootContext;
+
+public class ClayOreBlock extends Block
+{
+    public ClayOreBlock()
+    {
+        super(
+                Block.Properties.create(Material.ROCK)
+                    .hardnessAndResistance(1.5f)
+                    .harvestTool(ToolType.PICKAXE)
+                    .harvestLevel(1)
+                    .sound(SoundType.STONE)
+                );
+
+        setRegistryName(new ResourceLocation(ClayOre.modid, "clay_ore"));
+    }
+
+    @Override
+    public List<ItemStack> getDrops(BlockState state,
+            LootContext.Builder builder)
+    {
+        ArrayList<ItemStack> drop_list = new ArrayList<ItemStack>();
+        drop_list.add(new ItemStack(Items.CLAY_BALL, 5));
+        return drop_list;
+    }
+}
